392513f5c240d73300c1c27c3e29933e16cd79ec
Iván García <igarcia@commit.studio>
2022-02-20 15:28:13 -0300

    Added markdown support

--- app/routes/note/$id.tsx
+++ app/routes/note/$id.tsx
@@ -1,7 +1,8 @@
-import { Form, Link, redirect, useLoaderData } from 'remix'
+import { Form, Link, redirect, useLoaderData } from "remix";
 import type { ActionFunction, LoaderFunction } from "remix";
 import axios from "axios";
-import moment from 'moment';
+import moment from "moment";
+import { marked } from "marked";
 
 export const action: ActionFunction = async ({ request, params }) => {
   const api_url: string = process.env.API_URL;
@@ -11,7 +12,7 @@ export const action: ActionFunction = async ({ request, params }) => {
     if (method === "delete") {
       await axios.delete(`${api_url}/notes/${params.id}`);
     }
-    return redirect('/notes');
+    return redirect("/notes");
   } catch (error) {
     console.log(error);
     return {
@@ -24,31 +25,38 @@ export const loader: LoaderFunction = async ({ params }) => {
   const api_url: string = process.env.API_URL;
 
   try {
-    const note = await axios(api_url + '/notes/' + params.id);
-    return note.data;
+    const note = await axios(api_url + "/notes/" + params.id);
+    const MDcontent = marked(note.data.content);
+    return {
+      title: note.data.title,
+      content: MDcontent,
+      important: note.data.important,
+    };
   } catch (error) {
-    return {error: true};
+    return { error: true };
   }
-}
+};
 
 export default function SingleNote() {
   const data = useLoaderData();
 
-  if (data.error) return <p>Error fetching note</p>
+  if (data.error) return <p>Error fetching note</p>;
 
   return (
     <div>
-      <div className="links_header">
+      <div className='links_header'>
         <Link to='/notes'>Back</Link>
         <Link to={`/note/update/${data._id}`}>Edit</Link>
       </div>
       <h2>{data.title}</h2>
-      <small>{moment(data.createdAt).format('MMM DD, YYYY')}</small>
-      <p>{data.content}</p>
+      <small>{moment(data.createdAt).format("MMM DD, YYYY")}</small>
+      <div dangerouslySetInnerHTML={{
+        __html: data.content
+      }} />
       <Form method='post'>
-        <input type="hidden" name="_method" value="delete" />
-        <button type="submit">Delete</button>
+        <input type='hidden' name='_method' value='delete' />
+        <button type='submit'>Delete</button>
       </Form>
     </div>
-  )
+  );
 }
